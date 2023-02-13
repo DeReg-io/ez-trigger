ec41dea5c55a4ad207fc8afc045189f47fd7ad88
Philippe Dumonet <philogy@Philippes-MacBook-Air.local>
2023-02-13 16:40:04 +0100

    ✨ Deploy to testnet

---
+++ script/GoerliDeployTriggerable.s.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity 0.8.15;
+
+import {Script} from "forge-std/Script.sol";
+import {Test} from "forge-std/Test.sol";
+import {RepeatTriggerable} from "../test/mock/RepeatTriggerable.sol";
+
+/// @author philogy <https://github.com/philogy>
+contract GoerliDeployTriggerableScript is Script, Test {
+    function run() external {
+        uint256 managerDeployKey = vm.envUint("WORK_KEY");
+
+        vm.startBroadcast(managerDeployKey);
+
+        RepeatTriggerable triggerable = new RepeatTriggerable();
+
+        emit log_named_address("address(triggerable)", address(triggerable));
+
+        vm.stopBroadcast();
+    }
+}
---
+++ test/mock/RepeatTriggerable.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity 0.8.15;
+
+import {Triggerable} from "src/Triggerable.sol";
+
+/// @author philogy <https://github.com/philogy>
+contract RepeatTriggerable is Triggerable {
+    uint256 public triggeredCount;
+
+    event Triggered(uint256 count);
+
+    function _onEmergencyTrigger() internal override {
+        unchecked {
+            emit Triggered(triggeredCount++);
+        }
+    }
+}
