250ea3f83662a7e04d34b9b5c9761bea3cd0bb23
Philogy <philippe@dumo.net>
2023-04-17 12:37:18 +0200

    ✨ Add triggered owner

---
+++ src/presets/TriggeredOwner.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {Owned} from "solmate/auth/Owned.sol";
+import {Multicallable} from "solady/utils/Multicallable.sol";
+import {Triggerable} from "../Triggerable.sol";
+
+/// @author philogy <https://github.com/philogy>
+abstract contract TriggeredOwner is Owned, Multicallable, Triggerable {
+    constructor(address initialOwner, bytes memory deregInitUUID) Owned(initialOwner) Triggerable(deregInitUUID) {}
+
+    function relayCallTo(address to, bytes calldata cd, uint256 value, bool requireSuccess)
+        external
+        onlyOwner
+        returns (bytes memory retData)
+    {
+        bool success;
+        (success, retData) = to.call{value: value}(cd);
+        if (!success && requireSuccess) {
+            assembly {
+                revert(add(0x20, retData), mload(retData))
+            }
+        }
+    }
+}
