ac7ae8951a4c1a33f9a172e0a703dd76241a808b
Philogy <philippe@dumo.net>
2023-04-20 18:36:52 +0200

    ♻️ Remove pausable preset

--- src/presets/IntermediateTrigger.sol
+++ src/presets/IntermediateTrigger.sol
@@ -11,7 +11,7 @@ import {Triggerable} from "../Triggerable.sol";
  * that specific methods can be triggered. Owner of this contract can call any contract from this
  * contract. Requires the `_onTrigger` internal method to be implemented.
  */
-abstract contract TriggeredOwner is Owned, Multicallable, Triggerable {
+abstract contract IntermediateTrigger is Owned, Multicallable, Triggerable {
     constructor(address initialOwner, bytes memory deregInitUUID) Owned(initialOwner) Triggerable(deregInitUUID) {}
 
     receive() external payable {}
--- test/mock/Pausing.sol
+++ test/mock/Pausing.sol
@@ -3,11 +3,11 @@ pragma solidity 0.8.15;
 
 import {Pausable} from "@oz/security/Pausable.sol";
 import {Ownable} from "@oz/access/Ownable.sol";
-import {TriggerablePausable} from "../../src/presets/TriggerablePausable.sol";
+import {Triggerable} from "../../src/Triggerable.sol";
 
 /// @author philogy <https://github.com/philogy>
-contract Pausing is Pausable, Ownable, TriggerablePausable {
-    constructor(bytes memory _deregInitUUID) TriggerablePausable(_deregInitUUID) {}
+contract Pausing is Pausable, Ownable, Triggerable {
+    constructor(bytes memory _deregInitUUID) Triggerable(_deregInitUUID) {}
 
     function directPause() external onlyOwner {
         _pause();
@@ -15,5 +15,9 @@ contract Pausing is Pausable, Ownable, TriggerablePausable {
 
     function unpause() external onlyOwner {
         _unpause();
+    }
+
+    function _onTrigger() internal override {
+        _pause();
     }
 }
